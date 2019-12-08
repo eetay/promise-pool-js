c643e15d1f501eff9199930f3747ddcd8898407b
Eetay Natan <Eetay.Natan@harman.com>
2019-12-08 09:40:45 -0800

    added test for empty array of promises - has to be resolved

--- spec/promise-pool.spec.js
+++ spec/promise-pool.spec.js
@@ -156,33 +156,14 @@ test('20 promises; threads parallel 3', (done) => {
   })
 })
 
-test('20 promises; 1 thread', (done) => {
-  expect.assertions(2)
-  const numPromises = 20
+test('many threads, 0 promises', (done) => {
+  expect.assertions(1)
   const pool = promisePool({
-    max_parallel: 1,
-    next_promise: function ({index, data}) {
-      if (index>=numPromises) return null
-      return makePromise((index * 2) + data, randomTimeout())
-    },
-    next_promise_data: 17
+    max_parallel: 10,
+    promises: []
   })
   pool.then(function(result) {
-    expect(result.length).toBe(numPromises)
-    expect(result).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ context: expect.objectContaining({ index: 0, data: 17 }), result: 17 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 1, data: 17 }), result: 19 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 2, data: 17 }), result: 21 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 3, data: 17 }), result: 23 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 4, data: 17 }), result: 25 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 5, data: 17 }), result: 27 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 6, data: 17 }), result: 29 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 7, data: 17 }), result: 31 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 8, data: 17 }), result: 33 }),
-        expect.objectContaining({ context: expect.objectContaining({ index: 9, data: 17 }), result: 35 })
-      ])
-    )
+    expect(result.length).toBe(0)
     done()
   })
 })
